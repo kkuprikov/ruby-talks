027e294e9f6575749b08d402c560c63a9868fd5b
Kirill Kuprikov <k_kuprikov@gcore.lu>
2019-06-22 11:56:26 +0300

    Configured two types of value lookup

--- lib/ruby_talks/talk_loader.rb
+++ lib/ruby_talks/talk_loader.rb
@@ -1,4 +1,5 @@
-require 'pry'
+# frozen_string_literal: true
+
 require 'yaml'
 require 'nokogiri'
 require 'open-uri'
@@ -6,16 +7,54 @@ require 'open-uri'
 module RubyTalks
   class TalkLoader
     DEFAULT_MAPPING = {
-      title: '.presentation-content__title',
-      announce: '.presentation-content__description',
-      slides_url: '.presentation-materials__list',
-      video_url: '.presentation-video__link',
-      speaker: '.speaker__name',
-      speaker_bio: '.speaker__bio',
-      speaker_photo_url: '.speaker__img'
-    }
+      title: {
+        name: 'title',
+        css: '.presentation-content__title',
+        text_nodes: true,
+        value: 'content'
+      },
+      announce: {
+        name: 'announce',
+        css: '.presentation-content__description',
+        text_nodes: true,
+        value: 'content'
+      },
+      slides_url: {
+        name: 'slides_url',
+        css: '.presentation-materials__list',
+        search: 'a',
+        fetch: -1,
+        attr: 'href'
+      },
+      video_url: {
+        name: 'video_url',
+        css: '.presentation-video__link',
+        search: 'a',
+        fetch: -1,
+        attr: 'href'
+      },
+      speaker: {
+        name: 'speaker',
+        css: '.speaker__name',
+        text_nodes: true,
+        value: 'content'
+      },
+      speaker_bio: {
+        name: 'speaker_bio',
+        css: '.speaker__bio',
+        text_nodes: true,
+        value: 'content'
+      },
+      speaker_photo_url: {
+        name: 'speaker_photo_url',
+        css: '.speaker__img',
+        search: 'img',
+        fetch: -1,
+        attr: 'src'
+      }
+    }.freeze
 
-    def initialize base_url:, css_mapping: DEFAULT_MAPPING, url_marker: 'presentation'
+    def initialize(base_url:, css_mapping: DEFAULT_MAPPING, url_marker: 'presentation')
       @base_url = base_url
       @css_mapping = css_mapping
       @root_doc = Nokogiri::HTML(open(@base_url))
@@ -23,32 +62,32 @@ module RubyTalks
     end
 
     def load_conference
-      puts @root_doc.search('a').select{|element| presentation_path?(element['href']) }
-        .map{ |path| load_talk(path['href']) }
-        .to_yaml
+      puts @root_doc.search('a').select { |element| presentation_path?(element['href']) }
+                    .map { |path| load_talk(path['href']) }
+                    .to_yaml
     end
 
-    def load_talk path
+    def load_talk(path)
       doc = Nokogiri::HTML(open(URI.join(@base_url, path)))
-      slides = doc.css(@css_mapping[:slides_url]).search('a').last || {}
-      video = doc.css(@css_mapping[:video_url]).search('a').first || {}
-      photo = doc.css(@css_mapping[:speaker_photo_url]).search('img').last || {}
-
-      {
-        title: doc.css(@css_mapping[:title]).children.map{|c| c.text.strip}.join(' '),
-        announce: doc.css(@css_mapping[:announce]).children.map{|c| c.text.strip}.join(' '),
-        slides_url: slides['href'],
-        video_url: video['href'],
-        speaker: doc.css(@css_mapping[:speaker]).children.map{|c| c.text.strip}.join(' '),
-        speaker_bio: doc.css(@css_mapping[:speaker_bio]).children.map{|c| c.text.strip}.join(' '),
-        speaker_photo_url: photo['src']
-      }
+      @css_mapping.transform_values do |mapping|
+        attr_value(doc, mapping)
+      end
     end
 
     private
 
-    def presentation_path? url
+    def presentation_path?(url)
       url.include?(@url_marker)
+    end
+
+    def attr_value(doc, lookup_config)
+      target = doc.css(lookup_config[:css])
+      if tag = lookup_config[:search] # attribute value
+        elem = target.search(tag)[lookup_config[:fetch]] || {}
+        return elem[lookup_config[:attr]]
+      elsif lookup_config[:text_nodes]
+        elem = target.children.map { |c| c.public_send(lookup_config[:value]).strip }.join(' ')
+      end
     end
   end
 end
