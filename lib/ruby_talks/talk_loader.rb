bcf6a8ea4a5f5340ae092b4f626615b133376ee4
Kirill Kuprikov <k_kuprikov@gcore.lu>
2019-06-26 16:13:39 +0300

    Added conferences structure

--- lib/ruby_talks/talk_loader.rb
+++ lib/ruby_talks/talk_loader.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry'
 require 'yaml'
 require 'nokogiri'
 require 'open-uri'
@@ -7,15 +8,16 @@ require 'open-uri'
 module RubyTalks
   class TalkLoader
 
-    def initialize(base_url:, mapping_path:, url_marker: 'presentation')
-      @base_url = base_url
-      @css_mapping = YAML.load(File.read(mapping_path))
+    def initialize(config_path: )
+      config = YAML.load(File.read(config_path))
+      @base_url = config[:conference][:url]
+      @talk_marker = config[:conference][:talk_marker] || 'presentation'
+      @css_mapping = config[:mapping]
       @root_doc = Nokogiri::HTML(open(@base_url))
-      @url_marker = url_marker
     end
 
     def load_conference
-      puts @root_doc.search('a').select { |element| presentation_path?(element['href']) }
+      puts @root_doc.search('a').select { |element| presentation_path?(element['href']) }.first(2)
                     .map { |path| load_talk(path['href']) }
                     .to_yaml
     end
@@ -30,7 +32,8 @@ module RubyTalks
     private
 
     def presentation_path?(url)
-      url.include?(@url_marker)
+      return false unless url
+      url.include?(@talk_marker)
     end
 
     def attr_value(doc, lookup_config)
@@ -39,6 +42,7 @@ module RubyTalks
         elem = target.search(tag)[lookup_config[:fetch]] || {}
         return elem[lookup_config[:attr]]
       elsif lookup_config[:text_nodes]
+        binding.pry
         elem = target.children.map { |c| c.public_send(lookup_config[:value]).strip }.join(' ')
       end
     end
